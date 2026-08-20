3cd1b8775bad3d1615d5a6ba6885efdb95a48ea6
chelzinha <137534302+chelzinha@users.noreply.github.com>
2026-08-20 20:22:05 -0300

    Adiciona entrada separada do app Eleicoes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
   plugins: [netlify()],
+  build: {
+    rollupOptions: {
+      input: {
+        dce: resolve(__dirname, "index.html"),
+        eleicoes: resolve(__dirname, "eleicoes.html"),
+      },
+    },
+  },
   server: {
     port: 5173,
   },
